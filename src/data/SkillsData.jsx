252b2b60bf8963bc09e51e26f11d7c04e17b3ca6
Elias Hagos <eliasars@yahoo.com>
2025-01-13 18:02:20 +0200

    feat: Add images and data

---
+++ src/data/SkillsData.jsx
@@ -0,0 +1,47 @@
+import {
+    FaReact,
+    FaNodeJs,
+    FaPhp,
+    FaPython,
+    FaDocker,
+    FaAws,
+    FaDrupal
+  } from 'react-icons/fa';
+
+  export const skillsData = [
+    {
+      icon: <FaReact size={48} className="text-blue-500 mx-auto" />,
+      name: 'React.js',
+      level: 'Advanced'
+    },
+    {
+      icon: <FaNodeJs size={48} className="text-green-500 mx-auto" />,
+      name: 'Node.js',
+      level: 'Intermediate'
+    },
+    {
+      icon: <FaPhp size={48} className="text-purple-500 mx-auto" />,
+      name: 'PHP',
+      level: 'Intermediate'
+    },
+    {
+      icon: <FaDrupal size={48} className="text-blue-500 mx-auto" />,
+      name: 'Drupal',
+      level: 'Intermediate'
+    },
+    {
+      icon: <FaPython size={48} className="text-yellow-500 mx-auto" />,
+      name: 'Python',
+      level: 'Beginner'
+    },
+    {
+      icon: <FaDocker size={48} className="text-blue-400 mx-auto" />,
+      name: 'Docker',
+      level: 'Beginner'
+    },
+    {
+      icon: <FaAws size={48} className="text-orange-500 mx-auto" />,
+      name: 'AWS',
+      level: 'Beginner'
+    }
+  ];
